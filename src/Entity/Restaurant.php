89baf2ace7e848d33a2b61bc594a7c4fb3543aa4
Ivana Konta <ivana.konta99@gmail.com>
2024-02-27 17:52:44 +0100

    .

---
+++ src/Entity/Restaurant.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RestaurantRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RestaurantRepository::class)]
+class Restaurant
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $slug = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $contact = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $photo = null;
+
+    #[ORM\Column(length: 1000, nullable: true)]
+    private ?string $disclaimer = null;
+
+    #[ORM\Column(length: 1000, nullable: true)]
+    private ?string $termsAndConditions = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getContact(): ?string
+    {
+        return $this->contact;
+    }
+
+    public function setContact(string $contact): static
+    {
+        $this->contact = $contact;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(?string $photo): static
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    public function getDisclaimer(): ?string
+    {
+        return $this->disclaimer;
+    }
+
+    public function setDisclaimer(?string $disclaimer): static
+    {
+        $this->disclaimer = $disclaimer;
+
+        return $this;
+    }
+
+    public function getTermsAndConditions(): ?string
+    {
+        return $this->termsAndConditions;
+    }
+
+    public function setTermsAndConditions(?string $termsAndConditions): static
+    {
+        $this->termsAndConditions = $termsAndConditions;
+
+        return $this;
+    }
+}
